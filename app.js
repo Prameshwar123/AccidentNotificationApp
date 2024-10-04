ff0fe4506edc3c1313e169fde3afba359b95839d
Prameshwar123 <PrameshwarJakhad@gmail.com>
2024-10-04 18:33:09 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const { mongoose }  = require('mongoose');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //db connection
 mongoose.connect(process.env.MONGO_URL)
