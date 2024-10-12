3e1d01636e54728c8afdfa41c42fdc23f1539231
Prameshwar123 <PrameshwarJakhad@gmail.com>
2024-10-12 16:09:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const { mongoose }  = require('mongoose');
+const { mongoose } = require('mongoose');
+const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//db connection
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('Database Connected'))
     .catch((err) => console.log('Database not connected', err));
 
-//middleware
 app.use(express.json());
+
+const crypto = require('crypto');
+const secret = crypto.randomBytes(64).toString('hex');
+
+app.use(session({
+    secret: secret, 
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } 
+}));
 
 app.use('/', require('./routes/authRoute'));
 
