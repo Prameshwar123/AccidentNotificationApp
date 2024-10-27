ff8d68b5c71b9fb6b8c5f446a2a0a896d9b68426
Prameshwar123 <PrameshwarJakhad@gmail.com>
2024-10-27 15:11:36 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser, logoutUser, addContact, getContacts} = require('../controllers/authController')
+const {registerUser, loginUser, logoutUser, addContact, getContacts, deleteContact} = require('../controllers/authController')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
