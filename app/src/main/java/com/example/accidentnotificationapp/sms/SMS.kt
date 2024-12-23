a00ed4ecd6e15e8c3483622bbebac9cfc81772a4
Prameshwar123 <PrameshwarJakhad@gmail.com>
2024-12-23 19:35:11 +0530

    bug fixes and improvements

---
+++ app/src/main/java/com/example/accidentnotificationapp/sms/SMS.kt
@@ -0,0 +1,26 @@
+package com.example.accidentnotificationapp.sms
+
+import android.content.Context
+import android.telephony.SmsManager
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import com.example.accidentnotificationapp.data.Contact
+
+@Composable
+fun SendSMS(context: Context, contacts: List<Contact>, address: String) {
+	val smsManager = context.getSystemService(SmsManager::class.java)
+	val finalMessage: String = "Your family member got into an accident, " + address.ifEmpty {
+		 "Unable to get location."
+	}
+	try {
+		for (contact in contacts) {
+			smsManager.sendTextMessage(contact.phoneNumber, null, finalMessage, null, null)
+		}
+		Toast.makeText(context, "SMS sent to all numbers.", Toast.LENGTH_SHORT).show()
+	} catch (e: Exception) {
+		Log.d("current sms", finalMessage)
+		Log.e("SMS_ERROR", "Failed to send SMS: ${e.message}")
+		Toast.makeText(context, "Failed to send SMS.", Toast.LENGTH_SHORT).show()
+	}
+}
